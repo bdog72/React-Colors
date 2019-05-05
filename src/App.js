a20463c65db53aa3a9cf4758a5a764b52119c049
Brian Bycynski <high5bri@hotmail.com>
2019-05-05 06:09:45 -0400

    finished lecture 194

--- src/App.js
+++ src/App.js
@@ -18,11 +18,25 @@ class App extends Component {
       <Switch>
         <Route
           exact
+          path="/palette/:paletteId/:colorId"
+          render={routeProps => (
+            <SingleColorPalette
+              colorId={routeProps.match.params.colorId}
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.paletteId)
+              )}
+            />
+          )}
+        />
+
+        <Route
+          exact
           path="/"
           render={routeProps => (
             <PaletteList palettes={seedColors} {...routeProps} />
           )}
         />
+
         <Route
           exact
           path="/palette/:id"
@@ -33,11 +47,6 @@ class App extends Component {
               )}
             />
           )}
-        />
-        <Route
-          exact
-          path="/palette/:paletteId/:colorId"
-          render={() => <SingleColorPalette />}
         />
       </Switch>
       // <div>
